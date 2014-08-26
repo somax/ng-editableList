69e46180c68c0592b62462a443a197e122570ae9
somax <somaxj@gmail.com>
2014-08-26 20:30:24 +0800

    update myFilter

--- app/js/editableList.js
+++ app/js/editableList.js
@@ -1,15 +1,17 @@
 angular.module('app', [])
-	.factory('List', function ($http) {
+	.factory('List', function($http) {
 		var list = {
 			updateUrl: 'data/update.json',
 			dataUrl: 'data/list.json',
 			data: [],
-			get: function (_params) {
-				return $http.get(this.dataUrl, {params: _params}).success(function (_data) {
+			get: function(_params) {
+				return $http.get(this.dataUrl, {
+					params: _params
+				}).success(function(_data) {
 					list.data = _data;
 				});
 			},
-			update: function (_data) {
+			update: function(_data) {
 				// return $http.post(this.updateUrl, _data)
 				return $http.get(this.updateUrl, _data);
 			}
@@ -18,30 +20,62 @@ angular.module('app', [])
 		return list;
 	})
 
-	.factory('CountryList', function () {
-		return [
-			{"Country": 103, "CountryName": "伊拉克"},
-			{"Country": 104, "CountryName": "伊朗"},
-			{"Country": 105, "CountryName": "以色列"},
-			{"Country": 106, "CountryName": "意大利"},
-			{"Country": 107, "CountryName": "印度"},
-			{"Country": 108, "CountryName": "印度尼西亚"},
-			{"Country": 109, "CountryName": "英国"},
-			{"Country": 282, "CountryName": "英属维尔京群岛"},
-			{"Country": 110, "CountryName": "约旦"},
-			{"Country": 111, "CountryName": "越南"},
-			{"Country": 112, "CountryName": "赞比亚"},
-			{"Country": 114, "CountryName": "乍得"},
-			{"Country": 287, "CountryName": "直布罗陀"},
-			{"Country": 115, "CountryName": "智利"},
-			{"Country": 256, "CountryName": "中非"},
-			{"Country": 1, "CountryName": "中国"}
-		]
-	})
-	.filter('showCountryName', function (CountryList) {
-		return function (_countryId) {
-			if (!CountryList)return;
-			var _out = _countryId, i, country;
+.factory('CountryList', function() {
+	return [{
+		"Country": 103,
+		"CountryName": "伊拉克"
+	}, {
+		"Country": 104,
+		"CountryName": "伊朗"
+	}, {
+		"Country": 105,
+		"CountryName": "以色列"
+	}, {
+		"Country": 106,
+		"CountryName": "意大利"
+	}, {
+		"Country": 107,
+		"CountryName": "印度"
+	}, {
+		"Country": 108,
+		"CountryName": "印度尼西亚"
+	}, {
+		"Country": 109,
+		"CountryName": "英国"
+	}, {
+		"Country": 282,
+		"CountryName": "英属维尔京群岛"
+	}, {
+		"Country": 110,
+		"CountryName": "约旦"
+	}, {
+		"Country": 111,
+		"CountryName": "越南"
+	}, {
+		"Country": 112,
+		"CountryName": "赞比亚"
+	}, {
+		"Country": 114,
+		"CountryName": "乍得"
+	}, {
+		"Country": 287,
+		"CountryName": "直布罗陀"
+	}, {
+		"Country": 115,
+		"CountryName": "智利"
+	}, {
+		"Country": 256,
+		"CountryName": "中非"
+	}, {
+		"Country": 1,
+		"CountryName": "中国"
+	}]
+})
+	.filter('showCountryName', function(CountryList) {
+		return function(_countryId) {
+			if (!CountryList) return;
+			var _out = _countryId,
+				i, country;
 			for (i in CountryList) {
 				country = CountryList[i];
 				if (country.Country == _countryId) {
@@ -52,49 +86,67 @@ angular.module('app', [])
 			return _out;
 		}
 	})
-	//自定义过滤器
-	.filter('myFilter', function () {
-		return function (_in,_p,_c) {
-			// console.log('myFilter:',_in,_p,_c) ;
-			var _out=[];
-			for(var i=0;i<_in.length;i++){
-				if(_in[i].name=='AAA'){
-					_out.push(_in[i])
+//自定义过滤器
+.filter('myFilter', function() {
+	return function(_in, _filterWords) {
+		console.log('myFilter:', _in, _filterWords);
+		if (angular.isArray(_in)) {
+			_filterWords = _filterWords.toString();
+			if (_filterWords === '') {
+				return _in;
+			} else {
+				var _out = [];
+				_filterWords = _filterWords.replace(/ /gi, '|');
+
+				for (var i = 0; i < _in.length; i++) {
+
+					var _re = new RegExp(_filterWords, 'gi');
+
+					if (_re.test(_in[i].name)) {
+						_out.push(_in[i])
+					}
 				}
+				return _out;
 			}
-			return _out;
+
+
 		}
-	})
-	.controller('mainCtrl', function ($scope, List, CountryList) {
+
+	}
+})
+	.controller('mainCtrl', function($scope, List, CountryList) {
 		$scope.CountryList = CountryList;
 		$scope.editObj = null;
-		$scope.filterWords = 'AAA|BBB|CCC';
+		$scope.filterWords = 'AAA BBB CCC';
 		var currentObj = null;
 
 
-		var getList = $scope.getList = function () {
-			var params = {pageSize: 20, pageIndex: 2};
+		var getList = $scope.getList = function() {
+			var params = {
+				pageSize: 20,
+				pageIndex: 2
+			};
 			List.get(params)
-				.success(function (_data) {
-//					$scope.list = _data;
+				.success(function(_data) {
+					//					$scope.list = _data;
 					$scope.list = List.data;
 
 				})
-				.error(function (_data) {
+				.error(function(_data) {
 					//todo error callback
 				})
 		};
 
 
-		$scope.doEdit = function (_obj) {
+		$scope.doEdit = function(_obj) {
 			currentObj = _obj;
 			$scope.editObj = angular.copy(_obj);
 		};
 
-		$scope.doneEdit = function () {
+		$scope.doneEdit = function() {
 
 			List.update($scope.editObj)
-				.success(function (_data) {
+				.success(function(_data) {
 					var _editObj = $scope.editObj;
 					for (var _key in currentObj) {
 						if (_editObj.hasOwnProperty(_key)) {
@@ -104,14 +156,14 @@ angular.module('app', [])
 
 					$scope.editObj = null;
 				})
-				.error(function (_data) {
+				.error(function(_data) {
 					//todo error callback
 				})
 
 
 		};
 
-		$scope.cancelEdit = function () {
+		$scope.cancelEdit = function() {
 			$scope.editObj = null;
 		};
 
@@ -120,12 +172,15 @@ angular.module('app', [])
 
 
 		//提供内置过滤器filter的自定义过滤条件
-		$scope.myfilter = function(_in){
+		$scope.myfilter = function(_in) {
 			// console.log('myfilter::',_in);
-			var _re = new RegExp($scope.filterWords,'gi')
+			var _filterWords = $scope.filterWords.toString();
+			if (_filterWords.toString() === '') {
+				return true;
+			}
+			var _re = new RegExp(_filterWords.replace(/ /gi, '|'), 'gi')
 			// return _in.name.search(_re)>-1;
 			return _re.test(_in.name);
 		}
 
-	}
-);
+	});
